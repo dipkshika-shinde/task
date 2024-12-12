e50bed9ea05a1e9ef239960a3cc22c92dd39e11c
dipkshika-shinde <shindedipkshika@gmail.com>
2024-12-12 12:45:32 +0530

    Add files via upload

---
+++ display.js
@@ -0,0 +1,25 @@
+var arr=[
+    {
+        id:1,
+        description:'complete project',
+        priority:'low'
+    }, {
+        id:2,
+        description:'Incomplete project',
+        priority:'high'
+    },
+
+    {
+        id:3,
+        description:'Incomplete task',
+         priority:'low'
+    },
+    {
+        id:4,
+        description:'upload image',
+         priority:'high'
+    }
+    
+]
+
+console.log(arr);
